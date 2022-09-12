5df10704652407b8f28fa13eb831b0d0e777f5e3
Dementexo <104178225+Dementexo@users.noreply.github.com>
2022-09-11 20:04:22 -0500

    Add files via upload

---
+++ modeshift.js
@@ -0,0 +1,38 @@
+let themeButton = document.querySelector("#themeButton");
+let themeSwitchBackground = document.querySelector(".themeSlider");
+let pageBackground = document.querySelector(".siteContainer");
+let conditionHeader = document.querySelector(".conditionContainer");
+let conditionContainer1 = document.querySelector(".weatherConditionsData1");
+let conditionContainer2 = document.querySelector(".weatherConditionsData2");
+let conditionCircle = document.querySelector(".cpHolder");
+let searchBar1 = document.querySelector("#cityEntry");
+let searchBar2 = document.querySelector("#stateEntry");
+let searchB = document.querySelector("#searchButton");
+let tempSec = document.querySelector(".tempSection");
+let windSec = document.querySelector(".windSection");
+let weatherSec = document.querySelector(".weatherSection");
+let otherSec = document.querySelector(".otherSection");
+let containerLogos = document.getElementsByClassName(".secLogo");
+let conditionTempCircle = document.querySelector(".tempConditionUpdate");
+let tempLAndH = document.querySelector("#tempLowHigh");
+let data1 = document.querySelector(".ctData");
+let data2 = document.querySelector(".tlData");
+let data3 = document.querySelector(".thData");
+let currentMode = "dark";
+
+function themeChange(){
+    if(currentMode == "dark")
+    {
+        themeButton.style.marginLeft = "65%";
+        currentMode = "white";
+        return 0;
+    }
+    else if(currentMode == "white")
+    {
+        themeButton.style.marginLeft = "4%";
+        currentMode = "dark";
+        return 0;
+    }
+}
+
+themeButton.addEventListener('click', themeChange)
---
+++ weatherappAPI.js
@@ -0,0 +1,43 @@
+let searchButton = document.getElementById('searchButton');
+
+function dataFetch(){
+    let userCity = document.getElementById('cityEntry').value;
+    let userState = document.getElementById('stateEntry').value;
+    let apiKey = "160216139985f28ea2489dfd2c4ec5b4";
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "," + userState + "&appid=" + apiKey + "&units=imperial")
+        .then((response) => response.json())
+        .then((data) => displayWeather(data));
+}
+function displayWeather(data){
+    let conditionIcon = data.weather[0].icon;
+    let currentCondition = data.weather[0].main;
+    let locationLat = data.coord.lat;
+    let locationLon = data.coord.lon;
+    let currentTemp = data.main.temp;
+    let tempMin = data.main.temp_min;
+    let tempMax = data.main.temp_max;
+    let wcd1 = document.querySelector(".weatherConditionsData1");
+    let wcd2 = document.querySelector(".weatherConditionsData2");
+    document.querySelector("#conditionPicSRC").src = "https://openweathermap.org/img/wn/" + conditionIcon + ".png";
+    document.querySelector("#conditionPicSRC").style.visibility = "visible";
+    document.querySelector("#tcpSRC").src = "https://openweathermap.org/img/wn/" + conditionIcon + ".png";
+    document.querySelector("#tcpSRC").style.visibility = "visible";
+    wcd1.innerHTML = currentCondition;
+    wcd2.innerHTML = "Long: " + locationLon + " Lat: " + locationLat;
+    wcd1.style.width = "fit-content";
+    wcd2.style.width = "fit-content";
+    document.querySelector(".ctData").innerHTML = currentTemp + "°F";
+    document.querySelector(".tlData").innerHTML = tempMin + "°F";
+    document.querySelector(".thData").innerHTML = tempMax + "°F";
+    if (currentTemp >= 70){
+        document.querySelector(".tempConditionUpdate").style.backgroundColor = "rgb(255, 140, 0, 0.6)";
+    }
+    else if(currentTemp < 70 && currentTemp > 55){
+        document.querySelector(".tempConditionUpdate").style.backgroundColor = "rgb(100, 149, 237, 0.6)";
+    }
+    else if(currentTemp < 55){
+        document.querySelector(".tempConditionUpdate").style.backgroundColor = "rgb(0, 191, 255, 0.6)";
+    }
+}
+
+searchButton.addEventListener('click', dataFetch);
